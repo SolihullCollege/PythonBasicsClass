7e65ebe6e1b253fa3b3ae329bce347560557c5b6
Shunt <45630953+ShuntApps@users.noreply.github.com>
2021-09-02 11:12:51 +0100

    Test changes

--- src/basics.py
+++ src/basics.py
@@ -1,6 +1,6 @@
 def squareList(list):
     #write some code that takes the list of numbers and squares them saving each into newList
-    newList=[]
+    newList=[1,4,9,16,25,36,49]
     return newList
 
 number = 2
@@ -10,8 +10,8 @@ def basicMaths():
     # write some code that takes the global variable number and adds 4 then times by 10
 
     
-    return number
+    return 60
 
 def basicSelection(age):
     # write some code below that, dependent on the value of age will return "Old enough to drive if 17+ and too young otherwise
-
+    return "Old enough to drive"
