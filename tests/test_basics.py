141e706cf045ecf74bc7549ca90be3a0dbee551b
Shunt <45630953+ShuntApps@users.noreply.github.com>
2021-09-02 11:22:34 +0100

    src.basics update

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,14 +1,14 @@
 import pytest
-from src.basics import *
+import src.basics
 
 
 numList = [1, 2, 3, 4, 5, 6, 7]
 
 def test_square():
-    assert basics.squareList(numList) == [1,4,9,16,25,36,49]
+    assert src.basics.squareList(numList) == [1,4,9,16,25,36,49]
 
 def test_maths():
-    assert basics.basicMaths()== 60
+    assert src.basics.basicMaths()== 60
 
 def test_selection():
-    assert basics.basicSelection(17)=="Old enough to drive"
+    assert src.basics.basicSelection(17)=="Old enough to drive"
